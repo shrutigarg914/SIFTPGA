7daba1e4ec43663ba11059a0b39f17ea50c9f589
Evelyn Fu <evelynfu@mit.edu>
2023-11-19 19:46:49 -0500

    add the BRAM hdl lol, more gaussian pyramid stuff

--- hdl/top_level.sv
+++ hdl/top_level.sv
@@ -60,6 +60,7 @@ module top_level(
 
 
   // BUILDING THE GAUSSIAN PYRAMID
+  // Locating pyramid address STAGE 3
   parameter WIDTH = 128;
   parameter HEIGHT = 128;
   parameter BIT_DEPTH = 8;
@@ -71,6 +72,7 @@ module top_level(
   logic [2:0] blur_level; // horizontal location of current image in gaussian pyramid
   logic pyramid_done; // if we are done building the pyramid, stop increasing above addresses and stop writing to BRAM
   logic [7:0] resize_in;
+  logic pyramid_location_ready;
 
   // TEMP
   logic [BIT_DEPTH:0] pixel_in;
@@ -175,7 +177,7 @@ module top_level(
     .doutb(pixel_out_3)
   );
 
-  // STAGE 2
+  // Gaussian Blur STAGE 2
   logic[BIT_DEPTH*3-1:0] row1;
   logic[BIT_DEPTH*3-1:0] row2;
   logic[BIT_DEPTH*3-1:0] row3;
@@ -199,9 +201,41 @@ module top_level(
     .busy_out()
   );
 
-  // pipeline stages
-  logic kernel_data_ready; // STAGE 1
-  logic pyramid_location_ready; // STAGE 3
+  // Gathering kernel data STAGE 1
+  logic kernel_data_ready;
+  logic [1:0] kernel_x;
+  logic [1:0] kernel_y;
+
+  logic kernel_x_intermediate;
+  logic kernel_y_intermediate;
+  logic height_intermediate;
+
+  // get lookup address
+  always_comb begin
+    // saturate edges
+    if (center_addr_x == 0 && kernel_x == 0) begin
+      kernel_x_intermediate = center_addr_x;
+    end
+    if (center_addr_x == WIDTH-1 && kernel_x == 2) begin
+      kernel_x_intermediate = center_addr_x;
+    end
+    if (center_addr_y == 0 && kernel_y == 0) begin
+      kernel_y_intermediate = center_addr_y;
+    end
+    if (center_addr_y == WIDTH-1 && kernel_y == 2) begin
+      kernel_y_intermediate = center_addr_y;
+    end
+
+    // Get image height at current octave
+    case (pyramid_level)
+      0: height_intermediate = HEIGHT;
+      1: height_intermediate = HEIGHT / 2;
+      2: height_intermediate = HEIGHT / 4;
+      3: height_intermediate = HEIGHT / 8;
+    endcase
+
+    lookup_addr = kernel_x_intermediate + kernel_y_intermediate * height_intermediate;
+  end
 
   // TODO: Figure out how to signal collecting kernel pixels from BRAM, to waiting for blur module + storing result, to increasing center addr
   // Notes: Need to read 9 pixels, one at a time from BRAM (which takes 2 cycles each) to gather all kernel data
@@ -219,27 +253,47 @@ module top_level(
       pyramid_done <= 0;
     end else begin
       // Collect Kernel Pixels
+      if (pyramid_location_ready) begin // TODO: Add check that uart collection BRAM is ready
 
-      // Wait for blur module
+        // read out of right BRAM
+        case (pyramid_level)
+          0:
+            
+          1:
+
+          2:
+
+          3:
+        endcase
+      end
+
+      // Start blur module
+      if (kernel_data_ready) begin
+        blur_data_valid_in <= 1;
+      end
+
+      // TODO: SAVE OUTPUT TO PYRAMID
 
       // Increase center addr
-      if (center_addr_x < WIDTH) begin // Inc X
-        center_addr_x <= center_addr_x + 1;
-      end else begin
-        center_addr_x <= 0;
-        if (center_addr_y < HEIGHT) begin // Inc Y
-          center_addr_y <= center_addr_y + 1;
+      if (blur_data_valid_out) begin
+        if (center_addr_x < WIDTH) begin // Inc X
+          center_addr_x <= center_addr_x + 1;
         end else begin
-          center_addr_y <= 0;
-          if (blur_level < 4) begin // Inc blur level
-            blur_level <= blur_level + 1;
+          center_addr_x <= 0;
+          if (center_addr_y < HEIGHT) begin // Inc Y
+            center_addr_y <= center_addr_y + 1;
           end else begin
-            blur_level <= 0;
-            if (pyramid_level < 4) begin  // Inc pyramid level
-              pyramid_level <= pyramid_level + 1;
+            center_addr_y <= 0;
+            if (blur_level < 4) begin // Inc blur level
+              blur_level <= blur_level + 1;
             end else begin
-              pyramid_level <= 0;
-              pyramid_done <= 1;
+              blur_level <= 0;
+              if (pyramid_level < 4) begin  // Inc pyramid level
+                pyramid_level <= pyramid_level + 1;
+              end else begin
+                pyramid_level <= 0;
+                pyramid_done <= 1;
+              end
             end
           end
         end
